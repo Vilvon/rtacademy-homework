98c282f5c3e5d8731d2afed417aac203cb0634b1
Vilvon <davidcurby58@gmail.com>
2021-09-23 22:23:01 +0300

    homework

--- Blog/Pages/7.5.php
+++ Blog/Pages/7.5.php
@@ -2,52 +2,49 @@
 
 declare( strict_types=1 );
 
-$city_raw = $_GET['city'] ?? '';
+$city_draft = $_GET['city'] ?? '';
 
 function cityCapitalize( string $city ): string
 {
     $city = preg_replace( '#[^\sa-z-]#i', '', $city );
 
-    return
-        ucfirst(
-            strtolower(
-                trim( $city )
-            )
-        );
+    return ucfirst(strtolower(trim( $city )));
 }
 
 ?>
+
+
 <!DOCTYPE html>
 <html lang="uk">
 <head>
     <meta charset="utf-8">
-    <title>#7.5</title>
+    <title>7.5</title>
     <style>
         #city {
-            width : 15rem;
+            width : 15em;
+            margin-bottom: 2em ;
         }
-        .city {
-            padding    : 1rem;
-            margin     : 2rem 0;
-            background : #f0f0f0;
-        }
+
     </style>
 </head>
 <body>
+
     <form action="" method="GET">
-        <label for="city">Місто:</label>
-        <input type="text" id="city" name="city" value="<?= htmlspecialchars( $city_raw ) ?>" placeholder="Введіть місто (тільки латиниця)">
-        <button type="submit">Надіслати</button>
+        <label for="city">Город:</label>
+        <input type="text" id="city" name="city" value="<?= htmlspecialchars( $city_draft ) ?>">
+        <button type="submit">Отправить</button>
     </form>
+
+
     <?php
 
-    $city = cityCapitalize( $city_raw );
+    $city = cityCapitalize( $city_draft  );
 
     if( !empty($city) )
     {
 
     ?>
-    <div class="city"><?= $city ?></div>
+    <div><?= $city ?></div>
     <?php
 
     }
